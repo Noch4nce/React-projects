b3237717a59325880310f1af99544a5f58048bc3
Noch4nce <m4rgera123@mail.ru>
2022-08-19 20:07:12 +0500

    animated

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -14,25 +14,23 @@ const Modal = () => {
 				</button>
 			</div>
 
-			{isShowModal && (
-				<div className="overlay">
-					<div className="modal">
-						<svg
-							onClick={() => setIsShowModal(false)}
-							height="200"
-							viewBox="0 0 200 200"
-							width="200"
-						>
-							<title />
-							<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-						</svg>
-						<img
-							src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
-							alt="gif"
-						/>
-					</div>
+			<div className={`overlay animated ${isShowModal ? 'show' : ''}`}>
+				<div className="modal">
+					<svg
+						onClick={() => setIsShowModal(false)}
+						height="200"
+						viewBox="0 0 200 200"
+						width="200"
+					>
+						<title />
+						<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+					</svg>
+					<img
+						src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+						alt="gif"
+					/>
 				</div>
-			)}
+			</div>
 		</div>
 	)
 }
