1f851b55ad3629a36c9fae599d07864c9da78ff7
Noch4nce <m4rgera123@mail.ru>
2022-08-19 22:21:53 +0500

    quiz layout

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import './styles.scss'
+
+function Result() {
+	return (
+		<div className="result">
+			<img
+				src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+				alt="result"
+			/>
+			<h2>Вы отгадали 3 ответа из 10</h2>
+			<button>Попробовать снова</button>
+		</div>
+	)
+}
+
+const Game = () => {
+	return (
+		<>
+			<div className="progress">
+				<div style={{ width: '50%' }} className='progress__inner'/>
+			</div>
+			<h1>Что такое useState?</h1>
+			<ul>
+				<li>Это функция для хранения данных компонента</li>
+				<li>Это глобальный стейт</li>
+				<li>Это когда на ты никому не нужен</li>
+			</ul>
+		</>
+	)
+}
 
 const Quiz = () => {
-	return <div>ASDASD</div>
+	return (
+		<div className="App">
+			<Game />
+			{/*<Result />*/}
+		</div>
+	)
 }
 
 export default Quiz
