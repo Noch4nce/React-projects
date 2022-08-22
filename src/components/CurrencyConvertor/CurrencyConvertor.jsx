20fad281750cd152fdba094eb1f3f081a2be2d03
Noch4nce <m4rgera123@mail.ru>
2022-08-23 01:55:25 +0500

    layout, styles

--- src/components/CurrencyConvertor/CurrencyConvertor.jsx
+++ src/components/CurrencyConvertor/CurrencyConvertor.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Block from './Block'
 import './styles.scss'
 
 const CurrencyConvertor = () => {
-	return <div> asd</div>
+	return (
+		<div className="App">
+			<Block value={0} currency="RUB" />
+			<Block value={0} currency="USD" />
+		</div>
+	)
 }
 
 export default CurrencyConvertor
