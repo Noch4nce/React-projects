f46187644a5a64cb7f80fed805954e746809fe3e
Noch4nce <m4rgera123@mail.ru>
2022-08-22 14:23:50 +0500

    search

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,6 +7,7 @@ import './styles.scss'
 export const Users = () => {
 	const [userData, setUserData] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
+	const [searchValue, setSearchValue] = useState('')
 
 	useEffect(() => {
 		fetch(USERS_API)
@@ -25,7 +26,11 @@ export const Users = () => {
 				<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 					<path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
 				</svg>
-				<input type="text" placeholder="Найти пользователя..." />
+				<input
+					onChange={(event) => setSearchValue(event.target.value)}
+					type="text"
+					placeholder="Найти пользователя..."
+				/>
 			</div>
 			{isLoading ? (
 				<div className="skeleton-list">
@@ -35,9 +40,18 @@ export const Users = () => {
 				</div>
 			) : (
 				<ul className="users-list">
-					{userData.map((userInfo) => (
-						<User key={userInfo.id} {...userInfo} />
-					))}
+					{userData.map((userInfo) => {
+						if (
+							userInfo.first_name
+								.slice(0, searchValue.length)
+								.toLowerCase() === searchValue.toLowerCase() ||
+							userInfo.last_name
+								.slice(0, searchValue.length)
+								.toLowerCase() === searchValue.toLowerCase()
+						) {
+							return <User key={userInfo.id} {...userInfo} />
+						}
+					})}
 				</ul>
 			)}
 			<button className="send-invite-btn">Отправить приглашение</button>
