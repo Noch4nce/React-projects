0b9677f874afa798691114eedc90ff1da4e56a63
Noch4nce <m4rgera123@mail.ru>
2022-08-23 02:26:32 +0500

    api request

--- src/components/CurrencyConvertor/CurrencyConvertor.jsx
+++ src/components/CurrencyConvertor/CurrencyConvertor.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Block from './Block'
 import './styles.scss'
+import { CURRENCY_EXCHANGE_API } from '../api/api'
 
 const CurrencyConvertor = () => {
+	const [ratesData, setRatesData] = useState({})
+
+	useEffect(() => {
+		fetch(CURRENCY_EXCHANGE_API)
+			.then((data) => data.json())
+			.then((result) => setRatesData(result.rates))
+			.catch((err) => {
+				console.warn(err)
+				alert('Response error')
+			})
+	}, [])
+
+	console.log(ratesData, 'ratesData')
+
 	return (
 		<div className="App">
 			<Block value={0} currency="RUB" />
