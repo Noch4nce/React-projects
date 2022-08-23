a4fa3fd0a1961963c5e4dcb453f21244bdba2a67
Noch4nce <m4rgera123@mail.ru>
2022-08-23 14:25:17 +0500

    change currency

--- src/components/CurrencyConvertor/CurrencyConvertor.jsx
+++ src/components/CurrencyConvertor/CurrencyConvertor.jsx
@@ -5,6 +5,7 @@ import { CURRENCY_EXCHANGE_API } from '../api/api'
 
 const CurrencyConvertor = () => {
 	const [ratesData, setRatesData] = useState({})
+	const [currency, setCurrency] = useState('RUB')
 
 	useEffect(() => {
 		fetch(CURRENCY_EXCHANGE_API)
@@ -16,11 +17,17 @@ const CurrencyConvertor = () => {
 			})
 	}, [])
 
-	console.log(ratesData, 'ratesData')
+	const onChangeCurrency = (cur) => {
+		setCurrency(cur)
+	}
 
 	return (
 		<div className="App">
-			<Block value={0} currency="RUB" />
+			<Block
+				value={0}
+				currency={currency}
+				onChangeCurrency={onChangeCurrency}
+			/>
 			<Block value={0} currency="USD" />
 		</div>
 	)
